ee6f0bc3fe68a3825931d73591b71275a0789213
ZGS <zgstowell@gmail.com>
2022-06-02 15:32:39 -0700

    almost deployed?

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -4,7 +4,7 @@ import Todo from '../src/entities/Todo'
 export default new DataSource({
     type: "mysql",
     // host: "localhost",
-    port: 3306,
+    // port: 3306,
     // username: "root",
     // password: "password",
     // database: "typegql_todo",
