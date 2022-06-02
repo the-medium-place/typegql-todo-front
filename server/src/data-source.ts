b4adac143ccce064b0040c0f2496c8c38f7ba49e
ZGS <zgstowell@gmail.com>
2022-06-02 15:00:47 -0700

    setup deploy db

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -3,12 +3,13 @@ import Todo from '../src/entities/Todo'
 
 export default new DataSource({
     type: "mysql",
-    host: "localhost",
+    // host: "localhost",
     port: 3306,
-    username: "root",
-    password: "password",
-    database: "typegql_todo",
+    // username: "root",
+    // password: "password",
+    // database: "typegql_todo",
     entities: [Todo],
     synchronize: true,
     logging: false,
+    url: process.env.JAWSDB_URL
 })
