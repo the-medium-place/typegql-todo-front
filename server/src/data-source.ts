3c6ce52dbbf0b13f7635a952e45d219340a7331e
ZGS <zgstowell@gmail.com>
2022-06-02 21:29:17 -0700

    rearrange some stuff, wepack compiling now?

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -1,20 +1,15 @@
 import { DataSource } from "typeorm"
 import Todo from '../src/entities/Todo'
+require("dotenv").config()
 
 export default new DataSource({
     type: "mysql",
-    host: 'ebh2y8tqym512wqs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    host: process.env.REACT_APP_DB_HOST,
     port: 3306,
-    username: 'fe3xtfshyj5wqheb',
-    password: 'z7m0wk7dzc2xqiuw',
-    database: 'wlu9vapajvgft9ri',
-    // host: "localhost",
-    // port: 3306,
-    // username: "root",
-    // password: "password",
-    // database: "typegql_todo",
+    username: process.env.REACT_APP_DB_USERNAME,
+    password: process.env.REACT_APP_DB_PASSWORD,
+    database: process.env.REACT_APP_DB_DATABASE,
     entities: [Todo],
     synchronize: true,
     logging: false,
-    // url: process.env.JAWSDB_URL
 })
