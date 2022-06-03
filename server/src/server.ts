31f545816cded3d67c0c8bf54c8d481eaceebacf
ZGS <zgstowell@gmail.com>
2022-06-02 22:13:02 -0700

    update server pathing

--- server/src/server.ts
+++ server/src/server.ts
@@ -47,11 +47,11 @@ import AppDataSource from "./data-source";
   app.use(express.json());
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../../client/build')));
   }
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    res.sendFile(path.join(__dirname, '../../client/build/index.html'));
   });
 
   // app.get("/", (_req, res) => res.end("Welcome!"));
