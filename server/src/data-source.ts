0c2ed089c93d0626127f6ba3e22b295af6536239
ZGS <zgstowell@gmail.com>
2022-06-02 19:05:29 -0700

    trying tsc to compile typescript on deploy

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -3,6 +3,11 @@ import Todo from '../src/entities/Todo'
 
 export default new DataSource({
     type: "mysql",
+    host: 'ebh2y8tqym512wqs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    port: 3306,
+    username: 'fe3xtfshyj5wqheb',
+    password: 'z7m0wk7dzc2xqiuw',
+    database: 'wlu9vapajvgft9ri',
     // host: "localhost",
     // port: 3306,
     // username: "root",
@@ -11,5 +16,5 @@ export default new DataSource({
     entities: [Todo],
     synchronize: true,
     logging: false,
-    url: process.env.JAWSDB_URL
+    // url: process.env.JAWSDB_URL
 })
