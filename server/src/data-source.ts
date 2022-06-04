c5c8a3af04de6d6e0827eac45efb16cb803c1e88
ZGS <zgstowell@gmail.com>
2022-06-04 02:12:44 -0700

    deploying again i guess

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -5,11 +5,7 @@ require("dotenv").config()
 
 export default new DataSource({
     type: "mysql",
-    host: process.env.REACT_APP_DB_HOST,
-    port: 3306,
-    username: process.env.REACT_APP_DB_USERNAME,
-    password: process.env.REACT_APP_DB_PASSWORD,
-    database: process.env.REACT_APP_DB_DATABASE,
+    url: process.env.JAWSDB_URL,
     entities: [Todo, User],
     synchronize: true,
     logging: false,
